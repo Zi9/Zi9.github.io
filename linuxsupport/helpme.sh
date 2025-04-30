101b7ed74bf150ae2ff125ce8b8b9f6f0003c851
Zi9 <17174057+Zi9@users.noreply.github.com>
2025-05-01 02:32:50 +0300

    Update

--- linuxsupport/helpme.sh
+++ linuxsupport/helpme.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-wget -O /tmp/assist https://zi9.github.io/linuxsupport/linuxassist-tmate
+wget -O /tmp/assist https://zi9.dev/linuxsupport/linuxassist-tmate
 chmod +x /tmp/assist
 /tmp/assist -k tmk-jJd4n1DAqXERbwUqPl4UvEDtSd -n assist
