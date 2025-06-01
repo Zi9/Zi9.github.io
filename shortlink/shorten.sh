ce8d7ba0582224fe29f4501dadb1709f251f975d
Zi9 <17174057+Zi9@users.noreply.github.com>
2025-06-01 14:24:47 +0300

    Reorganize

---
+++ shortlink/shorten.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    >&2 echo "No link provided"
+    exit 1
+fi
+
+URL=$1
+
+if [ $# -eq 2 ]; then
+    FOLDER=$2
+else
+    FOLDER=$(find "$SHORTLINK_PATH" -type d | wc -l)
+fi
+
+SHORTLINK_PATH="/home/zi/dev/website/shortlink"
+mkdir -p "$SHORTLINK_PATH/$FOLDER/"
+cat >> "$SHORTLINK_PATH/$FOLDER/index.html"<< EOF
+<!DOCTYPE HTML>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0; url=$URL">
+    <script type="text/javascript">
+        window.location.href = "$URL"
+    </script>
+    <title>Shortlink via zi9.dev</title>
+</head>
+
+<body>
+    If you are not redirected automatically, follow this <a href='$URL'>link</a>.
+</body>
+
+</html>
+EOF
+
+git add "$SHORTLINK_PATH/$FOLDER/"
+git commit -m "Added short link $FOLDER"
+git push
+
+echo "===================="
+echo ""
+echo "Short link created! -> https://zi9.dev/shortlink/$FOLDER"
